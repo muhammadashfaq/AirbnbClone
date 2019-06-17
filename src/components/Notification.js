2198eba506c16b5fc098a2394553c3d92ff8809d
muhammadashfaq <ashfaq441997@gmail.com>
2019-06-17 06:31:40 -0700

    doing Logged in error message module

---
+++ src/components/Notification.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import { PropTypes } from 'prop-types';
+import colors from '../../styles/colors'
+
+class Notification extends Component {
+  render() {
+    return (
+      <div> textInComponent </div>
+    );
+  }
+}
+
+export default Notification;
